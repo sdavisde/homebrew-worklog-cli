b1f4f9b24af0d580ad258443075551c0d33dc41e
Sean Davis <sdavisde@gmail.com>
2025-12-06 00:00:05 -0600

    worklog-cli 0.3.3 (#2)
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/worklog-cli.rb
+++ Formula/worklog-cli.rb
@@ -1,12 +1,12 @@
 class WorklogCli < Formula
   desc "Daily task and time tracking with simple Markdown-based notes"
   homepage "https://github.com/sdavisde/worklog-cli"
-  version "0.3.0"
+  version "0.3.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sdavisde/worklog-cli/releases/download/v0.3.0/worklog-cli-aarch64-apple-darwin.tar.gz"
-      sha256 "b813e1ad7ed2bed2b0c66b14b6208e06d0ade1f09623da24ef449b072d2b48a4"
+      url "https://github.com/sdavisde/worklog-cli/releases/download/v0.3.3/worklog-cli-aarch64-apple-darwin.tar.gz"
+      sha256 "095e8a42d6a86436682b1b4aab88b6a80a42801c91c905c84999d0d30989923b"
     else
       odie "worklog-cli currently only supports ARM-based Macs (Apple Silicon). Please check the project homepage for platform availability updates."
     end
